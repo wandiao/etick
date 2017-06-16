a91ca54d29309ec4453aad89d30de6ca897ba64e
天下大雨 <851298395@qq.com>
2017-06-16 10:36:24 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const fileinclude  = require('gulp-file-include');
+const autoprefixer = require('gulp-autoprefixer');
+//编译less文件,兼容ie6
+gulp.task('less',function(){
+	gulp.src(['./res/less/style.less','./res/less/ie.less'])
+	.pipe(less())
+	.pipe(autoprefixer({
+        browsers: ['last 6 versions'],
+    }))
+	.pipe(gulp.dest('./res/css'))
+});
+// 适配所有html，排除include文件夹中html
+gulp.task('fileinclude', function() {
+    gulp.src(['./page/*.html','!./page/include/*.html'])
+        .pipe(fileinclude({
+          prefix: '@@',
+          basepath: '@file'
+        }))
+    .pipe(gulp.dest('webapp'));
+});
+gulp.task('default', function(){
+    gulp.run('less','fileinclude');
+	//监听文件变化
+    gulp.watch(['./res/less/*.less','./page/**/*.html'], function(){
+        gulp.run( 'less','fileinclude');
+    });
+});
---
+++ res/js/app.js
@@ -0,0 +1,34 @@
+$(function() {
+	//table斑马条纹
+	var tr = $('.table-striped tbody tr');
+	tr.each(function(index) {
+		if(index%2 !== 0) {
+			$(this).addClass('even');
+		}else{
+			$(this).addClass('odd');
+		}
+	})
+	//展示用户下拉选项
+	var cancelBtn = $('.cashier-record-list .cancel');
+	cancelBtn.on('click',function(e) {
+		$('.dialog-wrapper').show();
+		$('.modal').show();
+	})
+	$('.dialog .cancel').on('click',function() {
+		$('.dialog-wrapper').hide();
+		$('.modal').hide();
+	})
+	$('.u-wrapper').click(function() {
+		$('.u-option').toggle();
+	})
+	//
+	// 当前页面显示高亮
+	var currurl = location.href;
+	var urls = $('.menu-list li a');
+	urls.each(function(index,el) {
+		if(currurl.indexOf($(this).attr('href'))!= -1) {
+			$(this).addClass('active');
+		}
+	})
+	$('select').comboSelect();
+})
